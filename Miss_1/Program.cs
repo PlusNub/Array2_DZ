dbca9e6bef701fbcc456cb075c3769916d623826
Andrey Bukatish <poetrade@gmail.com>
2023-03-30 22:22:33 +0300

    двумерный массив заполненный случайными вещественными числами

---
+++ Miss_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[(int)i, (int)j],3}\t");
+        Console.WriteLine();
+    }
+}
+
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[rows, cols];
+FillArray(array);
+PrintArray(array);
